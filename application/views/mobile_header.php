44a0961e79fd237c1b75bf851b24049c05a4ddaa
DEP\Devri <devribudi@gmail.com>
2018-08-07 21:28:59 +0700

    Remove Search and Donate

--- application/views/mobile_header.php
+++ application/views/mobile_header.php
@@ -8,7 +8,7 @@
                             <nav id="dropdown">
                                 <ul>
                                     <li><a href="<?php echo base_url();?>">Beranda</a></li>
-                                    <li><a href="<?php echo base_url('About');?>">Profil</a></li>
+                                    <li><a href="<?php echo base_url('Profile');?>">Profile</a></li>
                                     <li><a href="<?php echo base_url('Berita');?>">Berita</a></li>
                                     <li><a href="<?php echo base_url('Kegiatan');?>">Kegiatan</a></li>
                                 </ul>
@@ -20,4 +20,3 @@
         </div>
     </div>
 </div>
-<!-- mobile-menu-area end -->
