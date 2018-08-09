c30c8c7a673d3f7d43f0b18eb036bbbb2b06ce0e
Mirana <devribudi@gmail.com>
2018-08-09 21:20:57 +0700

    Dynamic base url

--- application/views/berita_det_v.php
+++ application/views/berita_det_v.php
@@ -28,72 +28,71 @@
 <section class="blog-two-details-area ptb-80">
     <div class="container">
         <div class="row">
-            <div class="">
-                <div class="col-md-9 col-md-offset-1 commentContent">
-                    <div class="blog-container-inner">
-                        <!--                            <div class="post-for2">-->
-                        <!--                                <img src="-->
-                        <?php //echo base_url('assets/');?><!--img/blog/10.jpg" alt="">-->
-                        <!--                            </div>-->
-                        <div class="visual-inner">
-                            <h3 class="blog-title">
-                                Presidential Elections 2017
-                            </h3>
-                            <div class="blog-meta">
+            <div class="col-md-9 col-md-offset-1 commentContent">
+                <div class="blog-container-inner">
+                    <!--                            <div class="post-for2">-->
+                    <!--                                <img src="-->
+                    <?php //echo base_url('assets/');?><!--img/blog/10.jpg" alt="">-->
+                    <!--                            </div>-->
+                    <div class="visual-inner">
+                        <h3 class="blog-title">
+                            Presidential Elections 2017
+                        </h3>
+                        <div class="blog-meta">
                                         <span class="published3">
                                             <i class="fa fa-calendar" aria-hidden="true"></i> 26 June, 2017
                                         </span>
-                                <span class="published4"><i class="fa fa-comments-o" aria-hidden="true"></i> 01 Comment </span>
-                            </div>
-                            <div class="blog-content">
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its layout. The point of using Lorem Ipsum is that
-                                    it has a more-or-less normal. Dramatically cultivate viral paradigms after team
-                                    building e-services. Energistically network worldwide systems after highly efficient
-                                    innovation.Dramatically leverage other’s cross-unit expertise vis-a-vis
-                                    cross-platform ideas. Uniquely disintermediate cross-media synergy without fully
-                                    researched internal or “organic” sources. Monotonectally empower competitive
-                                    innovation after alternative core competencies. Professionally optimize turnkey
-                                    deliverables after enterprise bandwidth. Assertively foster inexpensive “outside the
-                                    box” thinking rather than intermandated niche markets.</p>
-                                <p class="blog-paragraph">Interactively implement intermandated architectures without
-                                    vertical methods of empowerment. Enthusiastically streamline multidisciplinary
-                                    e-business after frictionless schemas. Phosfluorescently fabricate intuitive
-                                    architectures through empowered manufactured products. Professionally repurpose
-                                    dynamic products whereas team driven process improvements. Authoritatively grow
-                                    leading-edge process improvements vis-a-vis cooperative internal or “organic”
-                                    sources.</p>
-                            </div>
+                            <span class="published4"><i class="fa fa-comments-o"
+                                                        aria-hidden="true"></i> 01 Comment </span>
                         </div>
-                        <div class="blog-tag">
-                            <p><i class="fa fa-tags" aria-hidden="true"></i><span class="tag">Tags:</span> Political,
-                                <span class="vidos2">Video</span>, Politico, Donate, Blog</p>
+                        <div class="blog-content">
+                            <p>It is a long established fact that a reader will be distracted by the readable
+                                content of a page when looking at its layout. The point of using Lorem Ipsum is that
+                                it has a more-or-less normal. Dramatically cultivate viral paradigms after team
+                                building e-services. Energistically network worldwide systems after highly efficient
+                                innovation.Dramatically leverage other’s cross-unit expertise vis-a-vis
+                                cross-platform ideas. Uniquely disintermediate cross-media synergy without fully
+                                researched internal or “organic” sources. Monotonectally empower competitive
+                                innovation after alternative core competencies. Professionally optimize turnkey
+                                deliverables after enterprise bandwidth. Assertively foster inexpensive “outside the
+                                box” thinking rather than intermandated niche markets.</p>
+                            <p class="blog-paragraph">Interactively implement intermandated architectures without
+                                vertical methods of empowerment. Enthusiastically streamline multidisciplinary
+                                e-business after frictionless schemas. Phosfluorescently fabricate intuitive
+                                architectures through empowered manufactured products. Professionally repurpose
+                                dynamic products whereas team driven process improvements. Authoritatively grow
+                                leading-edge process improvements vis-a-vis cooperative internal or “organic”
+                                sources.</p>
                         </div>
-                        <div class="blog-share">
-                            <p><i class="fa fa-share-alt" aria-hidden="true"></i><span class="tag">Share:</span></p>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-facebook"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-google-plus"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                    </div>
+                    <div class="blog-tag">
+                        <p><i class="fa fa-tags" aria-hidden="true"></i><span class="tag">Tags:</span> Political,
+                            <span class="vidos2">Video</span>, Politico, Donate, Blog</p>
+                    </div>
+                    <div class="blog-share">
+                        <p><i class="fa fa-share-alt" aria-hidden="true"></i><span class="tag">Share:</span></p>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
