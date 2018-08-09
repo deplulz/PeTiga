b537136c828d9747f60185efc3d454ebb09961d9
Mirana <devribudi@gmail.com>
2018-08-10 01:56:22 +0700

    Dynamic base url

--- assets/lib/home.js
+++ assets/lib/home.js
@@ -6,25 +6,27 @@
 		//---------------------------------------------
 			 $('#ensign-nivoslider').nivoSlider({
 				effect: 'random',
-				slices: 15,
+				slices: 2,
 				boxCols: 8,
 				boxRows: 4,
 				animSpeed: 500,
 				pauseTime: 5000,
 				startSlide: 0,
-				directionNav: true,
-				controlNavThumbs: false,
-				pauseOnHover: false,
+                //endSlide: 5,
+                directionNav: true,
+				controlNavThumbs: true,
+				pauseOnHover: true,
 				manualAdvance: true
 			 });
     $('#ensign-nivoslider-2').nivoSlider({
 				effect: 'random',
-				slices: 15,
+				slices: 2,
 				boxCols: 5,
-				boxRows: 4,
+    				boxRows: 4,
 				animSpeed: 500,
 				pauseTime: 5000,
 				startSlide: 0,
+				//endSlide: 5,
 				directionNav: true,
 				controlNavThumbs: false,
 				pauseOnHover: true,
