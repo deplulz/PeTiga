82dfd1f364d595098f482dd7592e03c9d56e957d
Mirana <devribudi@gmail.com>
2018-08-10 20:03:16 +0700

    Dynamic base url

--- application/views/keg_det_v.php
+++ application/views/keg_det_v.php
@@ -11,42 +11,45 @@
 </section>
 
 <!-- Slideshow container -->
-<div>
-    <div class="col-md-8">
-            <h1 class="TPos">Judul Berita</h1>
-    </div>
-
-    <div class="col-md-12 BPPos">
+<div class="col-md-12 ">
+    <div class="container">
         <div class="slideshow-container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1 class="TPos">Judul</h1>
+                    <span class="LUT">Last_Upadate_Time</span>
+                    <span class="LUB">Last_Upadate_By</span>
+                    <span class="LUB">Viewer_Counter</span>
+                </div>
 
-            <!-- Full-width images with number and caption text -->
-            <div class="mySlides fade">
-                <div class="numbertext">1 / 3</div>
-                <img src="<?php echo base_url('assets/'); ?>img/slider/4.jpg">
-                <div class="text">Caption Text</div>
+                <!-- Full-width images with number and caption text -->
+                <div class="mySlides fade" style="display: contents;">
+                    <img src="<?php echo base_url('assets/'); ?>img/slider/4.jpg">
+                    <div class="container text">Candi umCandi umbul merupakan sebuah kolam pemandian berbentuk persegi empat yang dibuat dari batu-batu komponen bangunan candi kuno. Pemandian  Air Hangat Candi Umbul memilki 2 buah kolam ukuran 12,5 x 7,15 meter dan kolam ukuran 8,5 x 7 meterCandi umbul merupakan sebuah kolam pemandian berbentuk persegi empat yang dibuat dari batu-batu komponen bangunan candi kuno. Pemandian  Air Hangat Candi Umbul memilki 2 buah kolam ukuran 12,5 x 7,15 meter dan kolam ukuran 8,5 x 7 meterCandi umbul merupakan sebuah kolam pemandian berbentuk persegi empat yang dibuat dari batu-batu komponen bangunan candi kuno. Pemandian  Air Hangat Candi Umbul memilki 2 buah kolam ukuran 12,5 x 7,15 meter dan kolam ukuran 8,5 x 7 meterbul merupakan sebuah kolam pemandian berbentuk persegi empat yang dibuat dari batu-batu komponen bangunan candi kuno. Pemandian  Air Hangat Candi Umbul memilki 2 buah kolam ukuran 12,5 x 7,15 meter dan kolam ukuran 8,5 x 7 meter</div>
+                </div>
+
+                <div class="mySlides fade" style="display: none;">
+                    <img src="<?php echo base_url('assets/'); ?>img/slider/2.jpg" style="width:100%">
+                    <div class="text">Caption Two</div>
+                </div>
+
+                <div class="mySlides fade" style="display: none;">
+                    <img src="<?php echo base_url('assets/'); ?>img/slider/2.jpg" style="width:100%">
+                    <div class="text">Caption Three</div>
+                </div>
+
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
             </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">2 / 3</div>
-                <img src="<?php echo base_url('assets/'); ?>img/slider/2.jpg" style="width:100%">
-                <div class="text">Caption Two</div>
-            </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">3 / 3</div>
-                <img src="<?php echo base_url('assets/'); ?>img/slider/2.jpg" style="width:100%">
-                <div class="text">Caption Three</div>
-            </div>
-
-            <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
-        <!-- The dots/circles -->
         <div class="pos">
             <span class="dot" onclick="currentSlide(1)"></span>
             <span class="dot" onclick="currentSlide(2)"></span>
             <span class="dot" onclick="currentSlide(3)"></span>
         </div>
     </div>
+
+    <!-- The dots/circles -->
+
 </div>
