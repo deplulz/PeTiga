7cf89d89d30ed4149dabe08489cba29c9067fe19
Mirana <devribudi@gmail.com>
2018-08-29 04:19:48 +0700

    hehe

--- application/controllers/Berita_detail.php
+++ application/controllers/Berita_detail.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Berita_Detail extends CI_Controller {
+class Berita_detail extends CI_Controller {
     public function index()
     {
         $data['title']='Berita';
